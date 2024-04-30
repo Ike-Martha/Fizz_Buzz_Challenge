e6ab6103a41e4038b333d70d08b8dca10657ff5d
Ike-Martha <amagwen36@gmail.com>
2024-04-30 14:57:51 +0100

    Add project files.

---
+++ Fizz_Buzz_Challenge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            for (int i = 1; i < 101; i++)
+            {
+                Consle.WriteLine(i);
+            }
+        }
+    }
+}
