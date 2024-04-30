663ec4c329dcaf726d813e95752ec4b5da894b50
Ike-Martha <amagwen36@gmail.com>
2024-04-30 15:11:26 +0100

    When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.

--- Fizz_Buzz_Challenge/Program.cs
+++ Fizz_Buzz_Challenge/Program.cs
@@ -8,7 +8,12 @@ namespace Application
         { 
             for (int i = 1; i < 101; i++)
             {
-                Consle.WriteLine(i);
+                //Consle.WriteLine(i);
+                if((i % 3 == 0) && (i % 5 == 1))
+                {
+                    Console.WriteLine($"{i} - FizzBuzz");
+                } 
+               
             }
         }
     }
